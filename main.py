aeec7f5183d67aeffb73fcf63c2a8d9c8982e812
Blake A Feucht <ama542blake@gmail.com>
2022-12-11 15:22:48 -0500

    Begin attempt to add songs to playlist
    Need to figure out how to do user authentication next. This fails because  no user authentication has been done.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# the absolute basics: https://discordpy.readthedocs.io/en/stable/intro.html#basic-concepts
+# the absolute basics of discordpy: https://discordpy.readthedocs.io/en/stable/intro.html#basic-concepts
 import os
-import re
 import discord
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
-from spotify_utils import SPOTIFY_BASE_URL, get_spotify_resource, resource_is_song
+from spotify_utils import SPOTIFY_BASE_URL, resource_is_song
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,9 +12,13 @@ DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 # set up discord API
 intents = discord.Intents.default()
 intents.message_content = True
-# creates the client connection to the Discord API
 discord_client = discord.Client(intents=intents)
-discord_client.run(DISCORD_TOKEN)
+
+# SpotifyClientCredentials() automatically reads the SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables
+spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+print(spotify_client.current_user())
+
+TEST_PLAYLIST_URL = os.getenv("TEST_PLAYLIST_URL")
 
 @discord_client.event
 async def on_ready():
@@ -29,8 +34,17 @@ async def on_message(message: discord.Message):
     if SPOTIFY_BASE_URL in message.content:
         # filter out parts of the message that aren't spotify
         song_urls = filter(lambda word: word.startswith(SPOTIFY_BASE_URL) and resource_is_song(word), message.content.split(' '))
-        song_resource_uris = map(lambda url: get_spotify_resource(url), song_urls)
-        for song_uri in song_resource_uris:
-            pass
+        test_playlist_id = spotify_client.playlist(TEST_PLAYLIST_URL)['id']
+        # TODO: https://www.newline.co/@bchiang7/breaking-down-oauth-with-spotify--90463996
+        spotify_client.playlist_add_items(test_playlist_id, song_urls)
+        print(f'\n\n{len(song_urls)} songs added')
+        for song_url in song_urls:
+            # https://developer.spotify.com/documentation/web-api/reference/#/operations/get-track
+            track = spotify_client.track(song_url)
+            await message.channel.send(f'You added the track {track["name"]} by {track["artists"][0]["name"]} with URI {track["uri"]}')
 
         return
+
+
+# creates the client connection to the Discord API
+discord_client.run(DISCORD_TOKEN)
--- spotify_utils.py
+++ spotify_utils.py
@@ -5,17 +5,16 @@ SPOTIFY_BASE_URL = "https://open.spotify.com/"
 valid_spotify_link = re.compile(r'https://[open.]?spotify.com/[track|song|playlist/artist]/')
 
 uri_split_re = re.compile(r'/|[?]')
-def get_spotify_resource(uri: str) -> tuple[str, str]:
+def get_spotify_resource(url: str) -> tuple[str, str]:
     """
     Takes a Spotify URI and return a tuple of (resource type, resource ID).
 
     :param uri: The Spotify URI to be parsed
     :return: A tuple (resource type, resourceID)
     """
-    split = uri_split_re.split(uri)
+    split = uri_split_re.split(url)
     return split[3], split[4]
 
 
-def resource_is_song(uri: str) -> bool:
-    return (get_spotify_resource(uri)[0] == 'track')
-    
+def resource_is_song(url: str) -> bool:
+    return (get_spotify_resource(url)[0] == 'track')
