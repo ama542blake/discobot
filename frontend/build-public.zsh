ef72f4700f9996e206043c8744dc478d398339dc
Blake A Feucht <ama542blake@gmail.com>
2022-12-18 00:56:19 -0500

    Add SASS to the project

--- frontend/build-public.zsh
+++ frontend/build-public.zsh
@@ -6,11 +6,19 @@
 GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 
-echo "${GREEN}COPY PUBLIC:${NOCOLOR} finished compiling TypeScript..."
 # copy all non-compiled files to the base directory for hosting
 echo "${GREEN}COPY PUBLIC:${NOCOLOR} copying HTML files..."
 cp -R ./src/index.html ./public/index.html
 echo "${GREEN}COPY PUBLIC:${NOCOLOR} finished copying HTML files"
+
+echo "${GREEN}COPY PUBLIC:${NOCOLOR} compiling SASS..."
+sass src/styles/
+cp -R src/styles/ public/css/   # move to the public directory
+rm public/css/index.scss
+rm src/styles/index.css
+rm src/styles/index.css.map
+echo "${GREEN}COPY PUBLIC:${NOCOLOR} done compiling SASS"
+
 echo "${GREEN}COPY PUBLIC:${NOCOLOR} copying CSS files..."
 cp -R ./src/css ./public/css
 echo "${GREEN}COPY PUBLIC:${NOCOLOR}: finished copying CSS files"
